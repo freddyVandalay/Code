09722e9bbdac39e3bc8d6472b8c4591116e6f793
Fredrik Sundström <fsund001@gold.ac.uk>
2017-03-01 10:49:22 +0000

    Edited the TestMyLevensthein.java

--- MyLevenshtein/TestMyLevensthein.java
+++ MyLevenshtein/TestMyLevensthein.java
@@ -8,20 +8,21 @@ class TestMyLevensthein{
     
     String word1;
     String word2;
+    int stringDistance;
     //Initialize the MyLevensthein class
     MyLevensthein test;
-    int stringDistance = test.distance();
+    
     
 
     //Program title and instructions
     System.out.println("----CHECK YOUR STRINGS WITH MyLevensthein----");
-  	System.out.println("This program is using the Levensthein algorithm");
-  	System.out.println("to compute the distance between to strings");
-  	
+    System.out.println("This program is using the Levensthein algorithm");
+    System.out.println("to compute the distance between to strings");
+    
     //User input
-  	System.out.print("Input a string: ");
+    System.out.print("Input a string: ");
     Scanner userInput = new Scanner(System.in);
-    word1=userInput.next();
+    word1 = userInput.next();
 
     //User input
     System.out.print("Input another string: ");
@@ -30,6 +31,8 @@ class TestMyLevensthein{
 
     //Compare strings
     test = new MyLevensthein(word1,word2);
+    stringDistance = test.distance();
+    System.out.println("The bottom right value in the matrix represents the end result.");
     System.out.println("The distance between the two strings are: " + stringDistance);
   }
 }
